573f2ddb090987f1b4b8c0141167abf957e187c9
Thomas Fitzgerald <tpfitz42@gmail.com>
2017-04-06 18:32:29 -0500

    printing to the dom

--- wysiwyg.js
+++ wysiwyg.js
@@ -13,23 +13,19 @@ function loopPeople(data){
   // console.log(data.People.length);
   var newCard = "";
   for(var i=0;i<data.People.length;i++){
-    // $("user-output").append("<p>Stuff</p>");
-    var newCard += `<div class="card">`;
-    var newCard += `<header class="card-header">${data.People[i].title}</header>`;
-    var newCard += `<article class="image"><img src"${data.People[i].image}"></article>`;
-    var newCard += `<section class="section">${data.People[i].name}`;
+    if(i%3===0){
+        newCard += `<div class="row">`;
+      }
+    newCard += `<div class="col-sm-6 col-md-4">`;
+    newCard += `<div class="card">`;
+    newCard += `<header class="card-header">${data.People[i].title}</header>`;
+    newCard += `<article class="image"><img src="${data.People[i].image}"></article>`;
+    newCard += `<section class="section">${data.People[i].name}`;
+    newCard += `</div></div>`;
+    if(i%3===2){
+        newCard += `</div>`;
+      }
   }
+  $("#user-output").append(newCard);
   console.log(newCard);
 }
-
-
-
-// newCard += `<div class="card" id="card-${[i]}">`;
-//     newCard += `<header class="card-header child">${people[i].title}</header>`;
-//     newCard += `<article class="image child"><img class="grand-child" src="${people[i].image}">`;
-//     newCard += `<section class="section grand-child">${people[i].name}<br /><p class="great-grand-child">${people[i].bio}</p></setion>`;
-//     // newCard += `<footer class="card-footer">${people[i].lifespan}</footer>`;
-//     newCard += `</article>`;
-//     newCard += `</div>`;
-
-// var inputValue = $("user-input")
